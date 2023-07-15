2c87418fef56cf42919794aaea9793df5cd23320
Dario Niero <dario.niero@enginius.com>
2023-07-15 11:49:43 +0200

    add user entity

---
+++ trains-delay/src/main/java/its/darioniero/trainsdelay/entities/User.java
@@ -0,0 +1,24 @@
+package its.darioniero.trainsdelay.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+}
