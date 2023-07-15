e2283bb1796e64307f6f88088dc35f2f4d0d35a7
Dario Niero <dario.niero@enginius.com>
2023-07-15 11:28:44 +0200

    init spring project

---
+++ trains-delay/src/main/java/its/darioniero/trainsdelay/TrainsDelayApplication.java
@@ -0,0 +1,13 @@
+package its.darioniero.trainsdelay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainsDelayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainsDelayApplication.class, args);
+	}
+
+}
---
+++ trains-delay/src/test/java/its/darioniero/trainsdelay/TrainsDelayApplicationTests.java
@@ -0,0 +1,13 @@
+package its.darioniero.trainsdelay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainsDelayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
